0915f9be89422b5682b21367e13800cececdeff3
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-05-29 03:26:41 +0300

    feat: add dark mode to dashboard, add guest layout to dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,21 +1,18 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+            <div class="flex justify-between text-white dark:text-black p-[10px]">
+                <h2 class="font-semibold text-xl flex-grow flex items-center justify-start">Dashboard</h2>
+            </div>
         </template>
 
-        <div class="py-12">
+        <div class="py-12 border-t">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"></div>
             </div>
         </div>
     </AppLayout>
