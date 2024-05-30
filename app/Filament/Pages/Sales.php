9a2fe3b8fe859215d778ffeb359aefb292706d29
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-05-30 14:49:48 +0300

    feat: add filament table for sales, add view action button for statistics, add header widgets

---
+++ app/Filament/Pages/Sales.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms;
+use App\Models\Sale;
+use Filament\Tables;
+use Filament\Pages\Page;
+use Filament\Tables\Table;
+use App\Filament\Widgets\SalesChart;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Concerns\InteractsWithTable;
+
+class Sales extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static ?string $navigationLabel = 'Sales';
+
+    protected static ?string $navigationGroup = 'Sales';
+
+    protected static string $view = 'filament.pages.sales';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            SalesChart::class,
+        ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Sale::query()->orderBy('productId', 'asc'))
+            ->columns([
+                Tables\Columns\TextColumn::make('productId')
+                    ->label('Id')
+                    ->toggleable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('product')
+                    ->sortable()
+                    ->toggleable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('productSlug')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('stock')
+                    ->badge()
+                    ->color(fn ($state) => $state > 100 ? 'green' : ($state < 10 ? 'red' : 'blue'))
+                    ->toggleable()
+                    ->numeric()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('description')
+                    ->wrap()
+                    ->toggleable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('category')
+                    ->badge()
+                    ->toggleable()
+                    ->color('zinc')
+                    ->sortable(),
+
+                Tables\Columns\ColorColumn::make('color')
+                    ->sortable()
+                    ->toggleable()
+                    ->copyable(),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('category')
+                    ->options(fn () => Sale::distinct()->pluck('category', 'category')->toArray()),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make()
+                    ->label('Statistics')
+                    ->modalHeading('View Statistics')
+                    ->form([
+                        Forms\Components\TextInput::make('sales')
+                            ->formatStateUsing(fn (string $state): string => number_format((int) $state)),
+
+                        Forms\Components\TextInput::make('revenue')
+                            ->formatStateUsing(fn (string $state): string => number_format((int) $state)),
+
+                        Forms\Components\TextInput::make('lastSaleDate'),
+                    ]),
+            ]);
+    }
+}
