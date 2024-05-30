632f80801811c1a6f8cc341432b6e3c7e94101e9
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-05-30 14:28:12 +0300

    refactor: remove flash messages

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -4,7 +4,6 @@ import { Head, Link, router } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
-import FlashMessage from "@/Components/FlashMessage.vue";
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
@@ -311,8 +310,6 @@ const logout = () => {
 
                 <!-- Page Heading -->
                 <header v-if="$slots.header" class="bg-black dark:bg-white shadow">
-                    <FlashMessage :message="$page.props.flash.message" :type="$page.props.flash.type" />
-
                     <div class="mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         <slot name="header" />
                     </div>
