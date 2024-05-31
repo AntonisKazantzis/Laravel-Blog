66674e278699165ede77d188ad4ee45d0683f203
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-06-01 02:55:45 +0300

    refactor: remove flash messages

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,10 +36,6 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
-            'flash' => [
-                'message' => fn () => $request->session()->get('message'),
-                'type' => fn () => $request->session()->get('type'),
-            ],
         ];
     }
 }
